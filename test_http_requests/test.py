8b1405af6946451c021fcd283adf1e45342d6654
thinceller <kohei.ct@outlook.jp>
2018-01-29 05:38:47 +0900

    pylintによる警告箇所を修正

--- test_http_requests/test.py
+++ test_http_requests/test.py
@@ -1,9 +1,9 @@
-import config
 from requests_oauthlib import OAuth1Session
+import config
 
 ### Constants
-OAUTH_KEY = {
-    "consumer_key": config.CONSUMER_KEY, 
+AUTH_KEY = {
+    "consumer_key": config.CONSUMER_KEY,
     "consumer_secret": config.CONSUMER_SECRET,
     "access_token": config.ACCESS_TOKEN,
     "access_token_secret": config.ACCESS_TOKEN_SECRET
@@ -11,24 +11,34 @@ OAUTH_KEY = {
 
 ### Functions
 def main():
+    """This is a main function.
+    """
     status = input("What's happening?: ")
     update_status(status)
 
-def create_oauth_session(oauth_key):
-    oauth = OAuth1Session(
-        oauth_key["consumer_key"],
-        oauth_key["consumer_secret"],
-        oauth_key["access_token"],
-        oauth_key["access_token_secret"]
+def create_oauth_session(auth_key):
+    """Performs authentication.
+
+    :param auth_key: Value and key for authentication
+    """
+    auth = OAuth1Session(
+        auth_key["consumer_key"],
+        auth_key["consumer_secret"],
+        auth_key["access_token"],
+        auth_key["access_token_secret"]
     )
-    return oauth
+    return auth
 
 def update_status(status):
-    url = "https://api.twitter.com/1.1/statuses/update.json"
-    params = { "status": status }
+    """Tweet content entered at the console.
 
-    oauth = create_oauth_session(OAUTH_KEY)
-    res = oauth.post(url, params = params)
+    :param status: Input text.
+    """
+    url = "https://api.twitter.com/1.1/statuses/update.json"
+    params = {"status": status}
+
+    auth = create_oauth_session(AUTH_KEY)
+    res = auth.post(url, params=params)
 
     # エラーが出た時はエラーコードを表示する
     if res.status_code != 200:
